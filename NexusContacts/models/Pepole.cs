2de99ed5c07af390c09d1104c421c1e54324abd2
Eitan-Oz <bachareitanoz@gmail.com>
2026-01-10 22:42:58 +0200

    Add AddContact window and improve Pepole data binding
    Added new AddContact WPF window and code-behind for adding contacts. Updated project file to include new window and ensure database files are always copied to output. Refactored Pepole class to properly implement INotifyPropertyChanged for WPF data binding. Performed minor project file cleanup.

--- NexusContacts/models/Pepole.cs
+++ NexusContacts/models/Pepole.cs
@@ -1,22 +1,56 @@
+using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices; // נדרש עבור [CallerMemberName]
 
+// איתן עוז בכר י"א 4
 namespace NexusContacts.models
 {
-    internal class Pepole: INotifyPropertyChanged
+    internal class Pepole : INotifyPropertyChanged
     {
+        // שדות פרטיים כדי שנוכל להפעיל את הודעת השינוי למסך
+        private string _firstName;
+        private string _lastName;
+        private string _phoneNumber;
+        private byte? _age;
+        private bool? _isFavorite;
+
         // הוספתי internal set כדי שתוכל למלא את ה-ID כשאתה מושך נתונים מה-SQL
         public int ContID { get; internal set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public byte? Age { get; set; }
+        public string FirstName
+        {
+            get => _firstName;
+            set { _firstName = value; OnPropertyChanged(); }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set { _lastName = value; OnPropertyChanged(); }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set { _phoneNumber = value; OnPropertyChanged(); }
+        }
+
+        public byte? Age
+        {
+            get => _age;
+            set { _age = value; OnPropertyChanged(); }
+        }
 
         // כאן ה-internal set מאפשר לקוד ה-SQL שלך להכניס את המידע
-       // public byte[] ImageRaw { get; internal set; }
-        public bool? IsFavorite { get; internal set; }
+        // public byte[] ImageRaw { get; internal set; }
+        public bool? IsFavorite
+        {
+            get => _isFavorite;
+            internal set { _isFavorite = value; OnPropertyChanged(); }
+        }
 
-        public Pepole(int ID, string Fname, string Lname, string phonNum,byte age,bool favo/*, byte[] getimag*/)
+        // הוספנו סימני שאלה לפרמטרים age ו-favo כדי שיתאימו ל-SQL
+        public Pepole(int ID, string Fname, string Lname, string phonNum, byte? age, bool? favo/*, byte[] getimag*/)
         {
             this.ContID = ID;
             this.FirstName = Fname;
@@ -24,20 +58,15 @@ namespace NexusContacts.models
             this.PhoneNumber = phonNum;
             this.Age = age;
             this.IsFavorite = favo;
-          //  this.ImageRaw = getimag;
+            //  this.ImageRaw = getimag;
         }
 
-        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
-        {
-            add
-            {
-                throw new System.NotImplementedException();
-            }
+        // --- המימוש המתוקן ל-PropertyChanged (במקום ה-throw) ---
+        public event PropertyChangedEventHandler PropertyChanged;
 
-            remove
-            {
-                throw new System.NotImplementedException();
-            }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
