0a33954628579ce64f84f26ccb54bf28f7c3a090
Eitan-Oz <bachareitanoz@gmail.com>
2026-01-10 14:12:07 +0200

    Update main window UI and add Peoples table SQL script
    Refactored MainWindow.xaml to use a grid layout with header, icon, and three action buttons. Added commented-out code for future button setup in MainWindow.xaml.cs. Introduced SQL script to create Peoples table for storing contact information.

---
+++ scripts/Create Peoples Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Peoples]
+(
+	[contID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Fname] NVARCHAR(50) NULL, 
+    [Lname] NVARCHAR(50) NULL, 
+    [PhoneNum] NVARCHAR(50) NULL, 
+    [IsFavorite ] BIT NULL
+)
