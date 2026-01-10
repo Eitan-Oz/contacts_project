f998291699d2cff28db970f43e4bed45adb708e1
Eitan-Oz <bachareitanoz@gmail.com>
2026-01-10 15:48:24 +0200

    UI redesign, language selection, new contact model
    Redesigned MainWindow UI with dynamic language selection and button creation. Added Hebrew credit text and updated greeting. Replaced static buttons with UniformGrid. Expanded Peoples SQL table to include age and image fields. Introduced Pepole.cs model to match new schema. Updated project file to include new model and adjusted database file handling. Added placeholder event handlers for future features.

---
+++ NexusContacts/models/Pepole.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace NexusContacts.models
+{
+    internal class Pepole: INotifyPropertyChanged
+    {
+        // הוספתי internal set כדי שתוכל למלא את ה-ID כשאתה מושך נתונים מה-SQL
+        public int ContID { get; internal set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public byte? Age { get; set; }
+
+        // כאן ה-internal set מאפשר לקוד ה-SQL שלך להכניס את המידע
+        public byte[] ImageRaw { get; internal set; }
+        public bool? IsFavorite { get; internal set; }
+
+        public Pepole(int ID, string Fname, string Lname, string phonNum,byte age,bool favo, byte[] getimag)
+        {
+            this.ContID = ID;
+            this.FirstName = Fname;
+
+        }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new System.NotImplementedException();
+            }
+
+            remove
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
