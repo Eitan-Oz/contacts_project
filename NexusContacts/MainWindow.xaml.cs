f2b0c95788fd0c4a7dce75a0d0ace345acd78b0c
school_PC_MAUI_V <soft@DESKTOP-305FGH>
2026-01-26 16:29:04 +0200

    add DB

--- NexusContacts/MainWindow.xaml.cs
+++ NexusContacts/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using NexusContacts.models;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -71,6 +73,44 @@ namespace NexusContacts
             favoriteShow.Show();
             this.Close();
         }
+        private static readonly HttpClient client = new HttpClient();
+        public async Task<string> GetGovDataAsync(string APIUrl)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(APIUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show($"Request exception: {e.Message}");
+                return null;
+            }
+        }
+        private async void  SetCity_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://data.gov.il/api/3/action/datastore_search?resource_id=8f714b6f-c35c-4b40-a0e7-547b675eee0e";
+            string jsonResult = await GetGovDataAsync(url);
+            if (jsonResult != null)
+            {
 
+              //  BaseDal dal = new BaseDal();
+              //  string sqlsand = "CREATE TABLE [Cities] (\n" +
+              //      "Id INT PRIMARY KEY,\n" +
+              //      "city_code INT,\n" +
+              //      "city_name_he NVARCHAR(100),\n" +
+              //      "city_name_en NVARCHAR(100)\n" +
+              //      ");";
+                   
+              //int esolt=  dal.ExecuteSelectIntQuery(sqlsand);
+                MessageBox.Show("I got the data"+jsonResult);
+            }
+            else
+            {
+                MessageBox.Show("We got aproblam to get the data");
+            }
+        }
     }
 }
---
+++ NexusContacts/models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusContacts.models
+{
+    internal class City
+    {
+
+        private int _id;
+        private string city_name_he;
+        private string city_name_en;
+    }
+}
